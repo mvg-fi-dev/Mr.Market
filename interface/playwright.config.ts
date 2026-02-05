04c1bd791d64cec3c5d6f011d12c1e1fc24cb1df
Zed Wong <kingflyme1@gmail.com>
2026-02-05 09:44:16 +0800

    Update interface test scripts to use bun instead of yarn

--- interface/playwright.config.ts
+++ interface/playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'yarn dev',
+      command: 'bun run dev',
       url: 'http://127.0.0.1:5173',
       reuseExistingServer: true,
       ignoreHTTPSErrors: true,
