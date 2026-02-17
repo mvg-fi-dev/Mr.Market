078a65f52174a2e64bb56c59d926977242f93d31
superclaw <mvgtothemoon@outlook.com>
2026-02-17 21:03:03 +0000

    test(validation): add class-validator decorators to message DTOs

--- server/src/modules/mixin/message/message.dto.ts
+++ server/src/modules/mixin/message/message.dto.ts
@@ -1,19 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class BroadcastMessageDto {
   @ApiProperty({ description: 'The text message to broadcast' })
+  @IsString()
+  @IsNotEmpty()
   message: string;
 }
 
 export class PrivateMessageDto {
   @ApiProperty({ description: 'The text message to send' })
+  @IsString()
+  @IsNotEmpty()
   message: string;
 
   @ApiProperty({ description: 'The user id of message receiver' })
+  @IsString()
+  @IsNotEmpty()
   user_id: string;
 }
 
 export class RemoveMessagesDto {
   @ApiProperty({ description: 'The message id array to remove' })
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
   message_ids: string[];
 }
